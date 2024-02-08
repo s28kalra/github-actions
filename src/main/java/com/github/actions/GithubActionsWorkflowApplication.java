248cedb8d7d4df093c7d7003bffacbf15637c208
Sagar Kalra <sagar_kalra@epam.com>
2024-02-08 12:07:58 +0530

    github-actions

---
+++ src/main/java/com/github/actions/GithubActionsWorkflowApplication.java
@@ -0,0 +1,21 @@
+package com.github.actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubActionsWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsWorkflowApplication.class, args);
+	}
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome To Docker Image";
+	}
+
+}
