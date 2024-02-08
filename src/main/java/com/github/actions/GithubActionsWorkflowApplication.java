4e2219bd3ea5e41ab54341e07690f7c6caaf12fc
Sagar Kalra <sagar_kalra@epam.com>
2024-02-08 12:55:18 +0530

    formatted

--- src/main/java/com/github/actions/GithubActionsWorkflowApplication.java
+++ src/main/java/com/github/actions/GithubActionsWorkflowApplication.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GithubActionsWorkflowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubActionsWorkflowApplication.class, args);
-	}
-	
-	@GetMapping("/welcome")
-	public String welcome() {
-		return "Welcome To Docker Image";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsWorkflowApplication.class, args);
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome To Docker Image";
+    }
 
 }
